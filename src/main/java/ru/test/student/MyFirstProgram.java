99098c2e8171bf314c92011e7ee207f202560ebd
KonstVa <v.konstantinov.im@gmail.com>
2020-03-15 22:13:51 +0300

    пробники

--- src/main/java/ru/test/student/MyFirstProgram.java
+++ src/main/java/ru/test/student/MyFirstProgram.java
@@ -4,6 +4,25 @@ public class MyFirstProgram {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        String somebody = "world";
+        hello("пробник 1");
+        hello("пробник 2");
+        hello("пробник 3 ");
+        double l = 5;
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area (a, b));
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    }
+    public static void hello(String somebody) // метод возвращает пустоту (ничего)
+    {
+        System.out.println("Hello, " + somebody + "!");
+    }
+    public static double area (double len){ // метод возращает результат умножения даблов, на вход принимает дабл
+        return len*len;
+    }
+
+    public static double area(double a, double b){ // метод возращает результат умножения даблов, на вход принимает 2 дабла
+        return a * b;
     }
 }
